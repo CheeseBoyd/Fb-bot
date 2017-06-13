5510c2a4d98ed81e786d50e19ccf49540e05357d
CheeseBoyd <ajlanot@gmail.com>
2017-06-13 10:50:29 +0800

    rewrite

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function receivedMessage(event) {
     // should match agains keywords
     switch (messageText) {
       case 'help':
-        quickReply(senderID, "you need help with?", "english", "math", "geometry", " science");
+        quickReply(senderID, "you need help with?", "english", "math", "geometry", "science");
       break;
       case 'generic':
         sendGenericMessage(senderID);
