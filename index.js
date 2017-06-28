dc438c264e416f2ed99dbd52719c3010f2ef4ae1
CheeseBoyd <ajlanot@gmail.com>
2017-06-28 09:53:25 +0800

    lousy programmer is still trying

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook', function (req, res) {
 
 function receivedMessage(event) {
 
-  var senderID = event.sender.id;
+  let senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
