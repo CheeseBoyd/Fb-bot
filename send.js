ea2ad8c97b06488d2869352e252c0fdefc9dbc1a
CheeseBoyd <ajlanot@gmail.com>
2017-07-31 12:22:30 +0800

    f

--- send.js
+++ send.js
@@ -64,39 +64,30 @@ var send = {
 	singleCard: function(senderID) {
 	  	var messageData = 
 		{
-		  "recipient":{
-		    "id":senderID
+		  recipient:{
+		    id:senderID
 		  },
-		  "message":{
-		    "attachment":{
-		      "type":"template",
-		      "payload":{
-		        "template_type":"generic",
-		        "elements":[
-		           {
-		            "title":"Welcome to Peter\'s Hats",
-		            "image_url":"https://petersfancybrownhats.com/company_image.png",
-		            "subtitle":"We\'ve got the right hat for everyone.",
-		            "default_action": {
-		              "type": "web_url",
-		              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-		              "messenger_extensions": true,
-		              "webview_height_ratio": "tall",
-		              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-		            },
-		            "buttons":[
-		              {
-		                "type":"web_url",
-		                "url":"https://petersfancybrownhats.com",
-		                "title":"View Website"
-		              },{
-		                "type":"postback",
-		                "title":"Start Chatting",
-		                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-		              }              
-		            ]      
-		          }
-		        ]
+		    message: {
+		      attachment: {
+		        type: "template",
+		        payload: {
+		          template_type: "generic",
+		          elements: [{
+		            title: 'New York Times',
+		            subtitle: 'Today\'s paper',
+		            item_url: 'https://www.nytimes.com/',               
+		            image_url: 'https://static01.nyt.com/images/icons/t_logo_291_black.png',
+		            buttons: [{
+		              type: "web_url",
+		              url: 'https://www.nytimes.com/',
+		              title: 'Visit page'
+		            }, {
+		              type: "postback",
+		              title: "Call Postback",
+		              payload: "Payload for first bubble",
+		            }]
+		          }]
+		        }
 		      }
 		    }
 		  }
